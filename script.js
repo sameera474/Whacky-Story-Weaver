e623a36904fbe9058b6d58b719a35b230aa3ff9a
Sameera Chaturanga Wagaarachchige <118694605+sameera474@users.noreply.github.com>
2024-03-09 20:54:17 +0200

    Add Restart Button

--- script.js
+++ script.js
@@ -17,23 +17,48 @@ const imageArray = [
   },
 ];
 
-// const story_images = document.querySelector("#story-images");
+const story_images = document.querySelector("#story-images");
 const story_display = document.querySelector("#story-display");
-const StartButton = document.querySelector("#StartButton");
+const startButton = document.querySelector("#startButton");
+
+function gameTrigerr() {
+  const startButtonStatus = startButton.textContent === "Start";
+  if (startButtonStatus) {
+    gameBoard();
+  } else {
+    gameRestart();
+  }
+}
 
 function gameBoard() {
-  for (let i = 0; i <= imageArray.length; i++) {
+  document.getElementById("question").textContent =
+    "What is the fovorite game you like?";
+
+  story_images.innerHTML = "";
+
+  for (let i = 0; i < imageArray.length; i++) {
     const imgStory = document.createElement("img");
     imgStory.setAttribute("src", imageArray[i].img);
-    imgStory.setAttribute("id", "frontImages");
+    imgStory.setAttribute("id", i);
+    imgStory.className = "frontImages";
     imgStory.setAttribute("onclick", "gamefuction()");
     console.log(imgStory);
 
-    document.getElementById("story-images").appendChild(imgStory);
+    story_images.appendChild(imgStory);
+    startButton.removeEventListener("click", gameBoard);
+    startButton.textContent = "Restart";
+    startButton.addEventListener("click", gameRestart);
   }
-  
-  const resetButton = 
+}
 
+function gameRestart() {
+  document.getElementById("question").textContent = "";
+  document.getElementById("story-images").innerHTML = "";
+  startButton.textContent = "Start";
+  startButton.removeEventListener("click", gameRestart);
+  startButton.addEventListener("click", gameBoard);
+
+  console.log("Game has been reset.");
 }
 
 function gamefuction() {
